846bbb619b870dd7dab18da6f48f6f1808a25314
NicholausND <nicholausd45@gmail.com>
2023-08-14 16:20:47 +0700

    Add project files.

---
+++ Arduino Interface/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Arduino_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.online = new System.Windows.Forms.Button();
+            this.offline = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.serialMonitorTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.inputBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.ItemHeight = 16;
+            this.comboBoxPort.Location = new System.Drawing.Point(29, 31);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(214, 24);
+            this.comboBoxPort.TabIndex = 0;
+            this.comboBoxPort.Text = "Choose Serial Port";
+            this.comboBoxPort.SelectedIndexChanged += new System.EventHandler(this.comboBoxPort_SelectedIndexChanged);
+            // 
+            // online
+            // 
+            this.online.BackColor = System.Drawing.SystemColors.Highlight;
+            this.online.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.online.Location = new System.Drawing.Point(29, 70);
+            this.online.Name = "online";
+            this.online.Size = new System.Drawing.Size(100, 50);
+            this.online.TabIndex = 1;
+            this.online.Text = "Connect";
+            this.online.UseVisualStyleBackColor = false;
+            this.online.Click += new System.EventHandler(this.online_Click);
+            // 
+            // offline
+            // 
+            this.offline.BackColor = System.Drawing.Color.IndianRed;
+            this.offline.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.offline.Location = new System.Drawing.Point(143, 70);
+            this.offline.Name = "offline";
+            this.offline.Size = new System.Drawing.Size(100, 50);
+            this.offline.TabIndex = 2;
+            this.offline.Text = "Disconnect";
+            this.offline.UseVisualStyleBackColor = false;
+            this.offline.Click += new System.EventHandler(this.offline_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(289, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 297);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Manual Input";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(95, 222);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(40, 40);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(174, 162);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(40, 40);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(95, 161);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(40, 40);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(16, 162);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(40, 40);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(174, 99);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(40, 40);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(95, 100);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(40, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBoxPort);
+            this.groupBox2.Controls.Add(this.offline);
+            this.groupBox2.Controls.Add(this.online);
+            this.groupBox2.Location = new System.Drawing.Point(12, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(271, 151);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Serial Connection";
+            // 
+            // sendButton
+            // 
+            this.sendButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.sendButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.sendButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.sendButton.Location = new System.Drawing.Point(85, 94);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(100, 40);
+            this.sendButton.TabIndex = 6;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = false;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // serialMonitorTextBox
+            // 
+            this.serialMonitorTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.serialMonitorTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serialMonitorTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.serialMonitorTextBox.Location = new System.Drawing.Point(6, 21);
+            this.serialMonitorTextBox.Name = "serialMonitorTextBox";
+            this.serialMonitorTextBox.ReadOnly = true;
+            this.serialMonitorTextBox.Size = new System.Drawing.Size(206, 241);
+            this.serialMonitorTextBox.TabIndex = 7;
+            this.serialMonitorTextBox.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.inputBox);
+            this.groupBox3.Controls.Add(this.sendButton);
+            this.groupBox3.Location = new System.Drawing.Point(12, 179);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(271, 140);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Pin Input";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(29, 21);
+            this.inputBox.Multiline = false;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(214, 68);
+            this.inputBox.TabIndex = 9;
+            this.inputBox.Text = "";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.serialMonitorTextBox);
+            this.groupBox4.Location = new System.Drawing.Point(552, 22);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(218, 297);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Serial Monitor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Arduino Control Interface";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Button online;
+        private System.Windows.Forms.Button offline;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.RichTextBox serialMonitorTextBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox inputBox;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
---
+++ Arduino Interface/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arduino_Interface
+{
+    public partial class Form1 : Form
+    {
+        private string receivedDataBuffer = "";
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] availablePorts = SerialPort.GetPortNames();
+            comboBoxPort.Items.AddRange(availablePorts);
+        }
+
+        private bool IsSerialPortOpen()
+        {
+            return serialPort != null && serialPort.IsOpen;
+        }
+        private void ShowSerialPortErrorMessage()
+        {
+            MessageBox.Show("Serial port is not open. Connect to the Arduino first.");
+        }
+        private void comboBoxPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string receivedData = serialPort.ReadExisting();
+            receivedDataBuffer += receivedData;
+
+            if (!receivedData.Contains("done"))
+            {
+                AppendDataToSerialMonitor(receivedData);
+            }
+        }
+
+
+        private void AppendDataToSerialMonitor(string data)
+        {
+            // Use the Invoke method to safely update the UI control from a different thread
+            if (serialMonitorTextBox.InvokeRequired)
+            {
+                serialMonitorTextBox.Invoke(new Action<string>(AppendDataToSerialMonitor), new object[] { data });
+            }
+            else
+            {
+                // Append the received data to the existing content of the UI control
+                serialMonitorTextBox.AppendText(data);
+            }
+        }
+
+
+
+        private void online_Click(object sender, EventArgs e)
+        {
+            string selectedPort = comboBoxPort.SelectedItem.ToString();
+            serialPort = new SerialPort(selectedPort, 115200); // Set appropriate baud rate and other settings
+            try
+            {
+                serialPort.Open();
+                if (serialPort.IsOpen)
+                {
+                    // Send initial data to the Arduino after the connection is established
+                    serialPort.Write("online");
+                    serialPort.DataReceived += SerialPort_DataReceived;
+                }
+                else
+                {
+                    MessageBox.Show("Failed to open the serial port.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private void offline_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.DataReceived -= SerialPort_DataReceived;
+                serialPort.Close();
+
+                serialPort.Dispose();
+            }
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("5");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("1");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("6");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("8");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("3");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("4");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("7");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("2");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("9");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                serialPort.Write("0");
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if (IsSerialPortOpen())
+            {
+                string inputText = inputBox.Text;
+
+                for (int i = 0; i < inputText.Length; i++)
+                {
+                    char c = inputText[i];
+                    serialPort.Write(c.ToString());
+
+                    while (!receivedDataBuffer.Contains("done"))
+                    {
+                        // Wait and allow the SerialPort_DataReceived event to handle the feedback
+                        Application.DoEvents();
+                    }
+
+                    // Clear the buffer after receiving feedback
+                    receivedDataBuffer = "";
+
+                    // If this is the last character, clear the inputBox
+                    if (i == inputText.Length - 1)
+                    {
+                        inputBox.Clear();
+                    }
+                }
+            }
+            else
+            {
+                ShowSerialPortErrorMessage();
+            }
+        }
+
+
+    
+
+       
+
+        private void inputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
